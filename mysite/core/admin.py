9224f1dd022b54f6674b8713c29043aa01916c54
Karel Verhoeven <karel.verhoeven@olx.com>
2017-03-02 14:34:26 +0200

    admin --user changes

--- mysite/core/admin.py
+++ mysite/core/admin.py
@@ -4,5 +4,14 @@ from django.contrib import admin
 from .models import Profile
 
 
+class ProfileAdmin(admin.ModelAdmin):
+	#fieldsets = [
+	#	(None,               {'fields': ['user']}),
+	#	('Date information', {'fields': ['location']}),
+	#]
+	#inlines = [ChoiceInline]
+	#list_filter = ['pub_date']
+	#search_fields = ['question_text']
+	list_display = ('user', 'location','birth_date','email_confirmed')
 
-admin.site.register(Profile)
+admin.site.register(Profile,ProfileAdmin)
